270befe43ffea74e92d462f4b07fd4f149c382e7
MEA\Aviral.Yadav <aviral.yadav@in.ey.com>
2018-07-22 13:50:22 +0530

    Added events on VR

--- Basics/index.vr.js
+++ Basics/index.vr.js
@@ -1,26 +1,12 @@
 import React, { Component } from 'react';
 
-import { AppRegistry, Pano, View, asset, Text } from 'react-vr';
+import { AppRegistry, Pano, View, asset } from 'react-vr';
 
 export default class Basics extends Component {
-  constructor(){
-    super();
-    this.state = {
-      fontSize: 0.1
-    }
-  }
   render() {
     return (
       <View>
         <Pano source={asset('starry-sky.jpg')} />
-        <Text 
-          style={{
-            fontSize: this.state.fontSize,
-            transform: [{translate: [0, 0, -2]}]
-          }}
-          onEnter={()=>this.setState({fontSize: 0.2})}
-          onExit={()=>this.setState({fontSize: 0.1})}
-        >Hover over me</Text>
       </View>
     );
   }
